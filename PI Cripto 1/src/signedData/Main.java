a278105e24700227288895aed36a7eb12fb5df4d
20.celso@gmail.com <20.celso@gmail.com@a04b24d9-0e15-1e1d-e336-09daf8477988>
2012-01-28 18:17:21 +0000

    SignedData com keystore a funcionar

--- PI Cripto 1/src/signedData/Main.java
+++ PI Cripto 1/src/signedData/Main.java
@@ -1,34 +1,22 @@
 package signedData;
 
 import java.security.MessageDigest;
-import java.security.Security;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1Sequence;
-import org.bouncycastle.asn1.ASN1Set;
-import org.bouncycastle.asn1.DERSequence;
-import org.bouncycastle.asn1.cms.ContentInfo;
-import org.bouncycastle.asn1.cms.SignedData;
-import org.bouncycastle.asn1.cms.SignerInfo;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Security.addProvider(new BouncyCastleProvider());
-
         String ksFile = "Recipient/ks_recipient", ks_type = "JCEKS", key_alias = "recipient_pkcs12", cert_alias = "cacert", algorithm = "RSA";
-        byte[] encrypted = null, decrypted, digest;
+        byte[] encrypted, decrypted, digest;
         Cifra cipher = new Cifra(algorithm);
         Digest dgst = new Digest("SHA-256", 32);
         RW_File rw;
         X509Certificate cert;
-        
+
 
         // Se tiverem sido passados três parâmetros ao programa
         if( args.length == 3) {
@@ -41,16 +29,16 @@ public class Main {
 
 
             try {
-                
+
 
                 /************************************/
                 /* Receber o Certificado do Emissor */
                 /************************************/
-                
+
 
                 /* Criar um objecto com o Certificado do Emissor */
                 cert = Certificate_Handler.getCertFromFile(args[0]);
-                
+
                 /* Verificar o certificado do emissor com a CA */
                 if(Certificate_Handler.verifyCertificate(RW_KeyStore.getCertificate(ksFile, ks_type, cert_alias), cert)) {
 
@@ -59,28 +47,17 @@ public class Main {
                     /***********************************/
                     /* Enviar o Certificado ao Emissor */
                     /***********************************/
-                    
+
+
 
                     rw = new RW_File(args[1]);
 
-
-
-
-                    SignedData sd = new SignedData( (ASN1Sequence) ((new ASN1InputStream((RW_KeyStore.getCertificate(ksFile, ks_type, key_alias)).getEncoded())).readObject()));
-                    ContentInfo ci = sd.getEncapContentInfo();  // retirar a mensagem do pacote
-
-                   // System.out.println(ci.getContent().toString());
-
-                    SignerInfo signerInfo = getSignerInfo(sd);
-
-                    
                     // ler os bytes do ficheiro que contém o resumo de mensagem encriptado
-                    // encrypted = (byte[]) proc.getContent();
+                    encrypted = rw.readByteFile();
 
                     // desencriptar o resumo de mensagem encriptado através da chave privada lida da keystore
                     decrypted = cipher.decifrar(encrypted, RW_KeyStore.export(ksFile, ks_type, key_alias, algorithm));
 
-System.out.println(new String(decrypted));
 
 
                     /* Definir o ficheiro da mensagem, de forma a recebê-la independentemente */
@@ -106,35 +83,5 @@ System.out.println(new String(decrypted));
         }
         else
             System.err.println("Invalid parameter number: " + args.length);
-    }
-
-    private static SignerInfo getSignerInfo(SignedData signedData) {
-
-        ASN1Set signerInfos = signedData.getSignerInfos();
-
-        if (signerInfos.size() > 1) {
-                System.err.println("WARNING: found " + signerInfos.size() + " signerInfos");
-        }
-
-        for (int i = 0; i < signerInfos.size(); i++) {
-
-            SignerInfo info = new SignerInfo((DERSequence)signerInfos.getObjectAt(i));
-                return info;
-        }
-
-        return null;
-    }
-
-    private static ASN1Sequence createCertificate(X509Certificate cert) throws Exception {
-
-        try {
-                byte[] certSpec = cert.getEncoded();
-                ASN1Sequence certSeq = (ASN1Sequence)(new ASN1InputStream(certSpec)).readObject();
-                return certSeq;
-        }
-        catch (Exception ioe) {
-            
-                throw new CertificateException("Could not construct certificate byte stream");
-        }
     }
 }
