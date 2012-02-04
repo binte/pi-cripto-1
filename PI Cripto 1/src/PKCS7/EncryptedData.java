20aa785c0a16f3ed426df9abbb71b63491290fa8
jpjc107@gmail.com <jpjc107@gmail.com@a04b24d9-0e15-1e1d-e336-09daf8477988>
2012-02-04 01:53:59 +0000

    EncryptedContentInfo, EncryptedData

---
+++ PI Cripto 1/src/PKCS7/EncryptedData.java
@@ -0,0 +1,51 @@
+/*
+ * * Contem a informação do conteúdo cifrado
+ * 
+ *              EncryptedData ::= SEQUENCE { version Version,
+ *                                            encryptedContentInfo EncryptedContentInfo }
+ *
+ */
+package PKCS7;
+
+import java.util.Set;
+import javax.mail.internet.MimePart;
+import org.bouncycastle.asn1.cms.RecipientInfo;
+
+
+/**
+ *
+ * @author joao
+ */
+public class EncryptedData extends Content{
+    
+    private int version;
+    private EncryptedContentInfo encryptedContentInfo;
+    
+    //Opcional
+    private String unprotectredAttrs;
+    
+    /**
+     * Basic Constructor
+     * @param version
+     * @param eci 
+     */
+    public EncryptedData(int version,EncryptedContentInfo eci){
+        super(version);
+        encryptedContentInfo = eci;
+    }
+    
+    /**
+     * Optional Constructor
+     * @param version
+     * @param attrs - Optional
+     * @param eci 
+     */
+    public EncryptedData(int version,String attrs,EncryptedContentInfo eci){
+        super(version);
+        unprotectredAttrs = attrs;
+        encryptedContentInfo = eci;
+    }
+    
+    
+    
+}
