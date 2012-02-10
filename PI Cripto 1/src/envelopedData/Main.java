d4e6e68c26f921981eed59deb3244ecc07566609
jpjc107@gmail.com <jpjc107@gmail.com@a04b24d9-0e15-1e1d-e336-09daf8477988>
2012-02-10 20:32:10 +0000

    Decifrar o criptograma com a chave secreta aes

--- PI Cripto 1/src/envelopedData/Main.java
+++ PI Cripto 1/src/envelopedData/Main.java
@@ -6,9 +6,13 @@ package envelopedData;
 import java.io.File;
 import java.io.IOException;
 import java.security.PrivateKey;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 import signedData.Cifra;
 import signedData.RW_File;
 import signedData.Gadgets;
+import signedData.RW_KeyStore;
 
 /**
  *
@@ -17,34 +21,49 @@ import signedData.Gadgets;
 public class Main {
 
     /**
-     * arg 1 - path do criptograma
+     * arg 1 - path do chave secreta encriptada
      * arg 2 - path da privateKey do receptor
+     * arg 3 - path do iv
+     * arg 4 - path do salt
+     * arg 5 - path do criptograma
      * @param args 
      */
     public static void main(String []args) throws IOException, Exception{
-        byte[] criptograma,textolimpo;
+        
+        String ksFile = "Recipient/ks_recipient", ks_type = "JCEKS",
+                key_alias = "recipient_pkcs12", cert_alias = "cacert", algorithm, provider = "BC";
+        char[] passphrase = {'1','2','3','4','5','6'};
+        byte[] criptograma,textolimpo,iv,salt,keydata;
         RW_File rw;
         Cifra cifra;
-        String provider = "BC";
-        String algorithm = "RSA";
+        algorithm = "AES/CBC";
+        
         PrivateKey prvtkey;
         File file;
         
-        //Leitura do criptograma
+        //Leitura do array da secretKey
         rw= new RW_File(args[0]);
+        keydata = rw.readByteFile();
+        SecretKeySpec sks = new SecretKeySpec (keydata,algorithm);
+        
+        
+        //leitura do iv
+        rw.setFile(args[2]);
+        iv = rw.readByteFile();
+        
+        //leitura do salt
+        
+        rw.setFile(args[3]);
+        salt = rw.readByteFile();
+        
+        //Leitura do criptograma
+        
+        rw.setFile(args[4]);
         criptograma = rw.readByteFile();
         
         //Leitura da private Key
-         System.out.println(args[0]);
-         System.out.println(args[1]);
-         file = new File(args[1]);
-         if(file.canRead()) System.out.println("Ok!");
-         prvtkey = Gadgets.readKeyPair(file).getPrivate();
-         
-         cifra = new Cifra(algorithm,provider);
-         textolimpo = cifra.decifrar(criptograma, prvtkey);
-        
-         System.out.println(new String(textolimpo));
-         
+        cifra  = new Cifra(algorithm,provider);
+        textolimpo = cifra.decifrar(keydata, sks);
+        System.out.println(new String(textolimpo));
         }
 }
